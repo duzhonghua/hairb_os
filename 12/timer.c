f168d894019f3245b5851b2497400afba672ee7c
duzhonghua <du.zhonghua@live.cn>
2017-01-05 16:18:35 +0800

    add 12 code

---
+++ 12/timer.c
@@ -0,0 +1,106 @@
+
+#include "bootpack.h"
+
+#define PIT_CTRL	0x0043
+#define PIT_CNT0	0x0040
+
+struct TIMERCTL timerctl;
+
+#define TIMER_FLAGS_UNINIT       0
+#define TIMER_FLAGS_ALLOC		1
+#define TIMER_FLAGS_USING		2	
+void init_pit(void)
+{
+	int i;
+	io_out8(PIT_CTRL, 0x34);
+	io_out8(PIT_CNT0, 0x9c);
+	io_out8(PIT_CNT0, 0x2e);
+	timerctl.count = 0;
+	timerctl.next = 0xffffffff; /*初始化*/
+	timerctl.using = 0;
+	for (i = 0; i < MAX_TIMER; i++) {
+		timerctl.timers0[i].flags = 0; /* 未使用 */
+	}
+	return;
+}
+
+struct TIMER *timer_alloc(void)
+{
+	int i;
+	for (i = 0; i < MAX_TIMER; i++) {
+		if (timerctl.timers0[i].flags == 0) {
+			timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+			return &timerctl.timers0[i];
+		}
+	}
+	return 0; 
+}
+
+void timer_free(struct TIMER *timer)
+{
+	timer->flags = 0; 
+	return;
+}
+
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data)
+{
+	timer->fifo = fifo;
+	timer->data = data;
+	return;
+}
+
+void timer_settime(struct TIMER *timer, unsigned int timeout)
+{
+	int e, i, j;
+	timer->timeout = timeout + timerctl.count;
+	timer->flags = TIMER_FLAGS_USING;
+	e = io_load_eflags();
+	io_cli();
+	/* 找到插入的位置*/
+	for (i = 0; i < timerctl.using; i++) {
+		if (timerctl.timers[i]->timeout >= timer->timeout) {
+			break;
+		}
+	}
+	/* 后边的都后移*/
+	for (j = timerctl.using; j > i; j--) {
+		timerctl.timers[j] = timerctl.timers[j - 1];
+	}
+	timerctl.using++;
+	/*插入*/
+	timerctl.timers[i] = timer;
+	timerctl.next = timerctl.timers[0]->timeout;
+	io_store_eflags(e);
+	return;
+}
+
+void inthandler20(int *esp)
+{
+	int i, j;
+	io_out8(PIC0_OCW2, 0x60);	/* 通知PIC IRQ0处理完毕 */
+	timerctl.count++;
+	if (timerctl.next > timerctl.count) {
+	 	/* 没有超时 */
+		return;
+	}
+	for (i = 0; i < timerctl.using; i++) {
+		/* 查找超时timers */
+		if (timerctl.timers[i]->timeout > timerctl.count) {
+			break;
+		}
+		/* 超时 */
+		timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
+		fifo8_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
+	}
+	/* i个timer超时，移动其余timer */
+	timerctl.using -= i;
+	for (j = 0; j < timerctl.using; j++) {
+		timerctl.timers[j] = timerctl.timers[i + j];
+	}
+	if (timerctl.using > 0) {
+		timerctl.next = timerctl.timers[0]->timeout;
+	} else {
+		timerctl.next = 0xffffffff;
+	}
+	return;
+}
